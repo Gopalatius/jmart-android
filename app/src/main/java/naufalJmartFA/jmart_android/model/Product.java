7707f252aa47fc21e1114e0667381d0ea1cfa571
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-05 00:37:36 +0700

    PT Modul 9 - Model PostTest sudah

---
+++ app/src/main/java/naufalJmartFA/jmart_android/model/Product.java
@@ -0,0 +1,19 @@
+package naufalJmartFA.jmart_android.model;
+
+public class Product extends Serializable{
+    public int accountId;
+    public ProductCategory category;
+    public boolean conditionUsed;
+    public double discount;
+    public String name;
+    public double price;
+    public byte shipmentPlans;
+    public int weight;
+
+    public String toString(){
+//        return "Name: "+name+"\nWeight: "+weight+"\nconditionUsed: "+conditionUsed+
+//                "\nprice: "+price+"\ndiscount: "+discount+"\nCategory: "+category+
+//                "\naccountId: "+accountId;
+        return "Name: "+name;
+    }
+}
