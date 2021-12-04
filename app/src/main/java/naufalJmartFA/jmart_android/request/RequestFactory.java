b90afaa18a7cf1981042f9405dcdfaf9cfbac929
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-05 00:40:57 +0700

    PT Modul 9 - RequestFactory.java sudah

--- app/src/main/java/naufalJmartFA/jmart_android/request/RequestFactory.java
+++ app/src/main/java/naufalJmartFA/jmart_android/request/RequestFactory.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 public class RequestFactory {
     private static final String URL_FORMAT_ID = "http://10.0.2.2:6969/%s/%d";
-    private static final String URL_FORMAT_PAGE= "http://10.0.2.2:6969/%s/%d";
+    private static final String URL_FORMAT_PAGE= "http://10.0.2.2:6969/%s/page?=page=%s&pageSize=%s";
 
     public static StringRequest getById(
             String parentURI,
@@ -30,12 +30,10 @@ public class RequestFactory {
     )
     {
         String url = String.format(URL_FORMAT_PAGE,parentURI);
-        Map<String,String> params = new HashMap<>();
-        params.put("page", String.valueOf(page));
-        params.put("pageSize", String.valueOf(pageSize));
-        return new StringRequest(Request.Method.GET, url, listener, errorListener){
-            public Map<String,String> getParams() { return params; }
-        };
+//        Map<String,String> params = new HashMap<>();
+//        params.put("page", String.valueOf(page));
+//        params.put("pageSize", String.valueOf(pageSize));
+        return new StringRequest(Request.Method.GET, url, listener, errorListener);
     }
 
 }
