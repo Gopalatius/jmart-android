1d709bde8c78713583e171c777036b62a7a15b79
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-16 00:50:07 +0700

    Proyek Akhir - Menu sudah sedikit jadi

--- app/src/main/java/naufalJmartFA/jmart_android/LoginActivity.java
+++ app/src/main/java/naufalJmartFA/jmart_android/LoginActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,7 +12,6 @@ import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 
@@ -59,6 +57,9 @@ public class LoginActivity extends AppCompatActivity {
                     passwordLogin.getText().toString(), listener, errorListener);
             RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
             requestQueue.add(loginRequest);
+            emailLogin.setText("");
+            passwordLogin.setText("");
+
         });
         registerNow.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
     }
--- app/src/main/java/naufalJmartFA/jmart_android/MainActivity.java
+++ app/src/main/java/naufalJmartFA/jmart_android/MainActivity.java
@@ -1,8 +1,16 @@
 package naufalJmartFA.jmart_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import naufalJmartFA.jmart_android.model.Account;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_activity, menu);
+        Account account = LoginActivity.getLoggedAccount();
+
+        if (account.store == null){
+            MenuItem addBox = findViewById(R.id.add_box_menu);
+            addBox.setVisible(false);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.search_menu:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.add_box_menu:
+                Toast.makeText(this, "Add box", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.person_menu:
+                Toast.makeText(this, "About me", Toast.LENGTH_SHORT).show();
+                Intent aboutMeIntent = new Intent(MainActivity.this,AboutMeActivity.class);
+                startActivity(aboutMeIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
