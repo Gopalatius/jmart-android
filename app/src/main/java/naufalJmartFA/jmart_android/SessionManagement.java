d6276ce5d48cc5d4b97d28f951868fe28782f10d
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-18 10:23:07 +0700

    Proyek Akhir - Javadoc for method in SessionManagement.java

--- app/src/main/java/naufalJmartFA/jmart_android/SessionManagement.java
+++ app/src/main/java/naufalJmartFA/jmart_android/SessionManagement.java
@@ -16,20 +16,34 @@ public class SessionManagement {
     String SHARED_PREF_NAME = "session";
     String SESSION_KEY = "session_user";
 
+    /**
+     * Default constructor for sessionManagement
+     * @param context which activity context
+     */
     public SessionManagement(Context context){
         sharedPreferences = context.getSharedPreferences(SHARED_PREF_NAME,Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
     }
 
+    /**
+     * When logged in, saveSession should be called
+     * @param account account credential for ID
+     */
     public void saveSession (Account account){
         editor.putInt(SESSION_KEY, account.id+1).commit();
     }
 
+    /**
+     * Get session to see if session already saved or not
+     * @return 0 if session isn't saved. Else, account.id + 1
+     */
     public int getSession(){
         return sharedPreferences.getInt(SESSION_KEY,0);
     }
 
-
+    /**
+     * Called when logged out
+     */
     public void removeSession(){
         editor.putInt(SESSION_KEY, 0).commit();
     }
